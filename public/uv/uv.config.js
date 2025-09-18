b89a97cb153dc10f2ad5c7ad0dedf7c170a1ff6b
landonalts <1630306@sweetwaterschools.net>
2025-09-18 14:21:42 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,9 @@
-// This file overwrites the stock UV config.js
-
-self.__uv$config = {
-  prefix: "/uv/service/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+export default {
+  prefix: '/service/',
+  bare: '/bare/',
+  encodeUrl: true,
+  tab: true,
+  // Optional settings
+  injectScripts: true,
+  rewriteCookies: true,
 };
